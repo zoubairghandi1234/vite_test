62d801bd908b1afbb27dcaacfe377231d0c2eaa2
zoubair ghandi <zoubair.ghandi@gmail.com>
2024-05-24 22:46:28 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Food from "./Food";
+import Footer from "./Footer";
+import Header from "./Header"
+
+function App() {
+  return (
+    <>
+       <Header/>
+       <Food/>
+       <Footer/>
+    </>
+  );
+}
+
+export default App
---
+++ src/Food.jsx
@@ -0,0 +1,18 @@
+
+
+function Food(){
+
+    const food1 = "Orange";
+    const food2 = "Banana";
+
+    return(
+   
+        <ul>
+            <li>Appel</li>
+            <li>{food1}</li>
+            <li>{food2}</li>
+        </ul>
+
+    );
+}
+export default Food
